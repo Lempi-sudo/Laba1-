01d01a0cc3bcea8b6531ec698b4aa349ca63e9a7
Andrei <andreii.lapschow1999@mail.ru>
2020-10-12 08:54:11 +0400

    added class StreamTable.h for output date in a table

--- List_Three.h
+++ List_Three.h
@@ -9,6 +9,18 @@ namespace Three_nsp {
 	class List_Three :public vector<Three*>
 	{
 	public:
+		double midlle_Alfa() const;
 	
 	};
+
+	double List_Three::midlle_Alfa() const
+	{
+		double A = 0;
+		for (int i = 0; i < this->size(); ++i)
+		{
+			A += this->at(i)->alfa();
+		}
+		double midlle_Alfa = A / this->size();
+		return midlle_Alfa;
+	}
 }
--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,8 @@
 #include <ctime>
 #include "windows.h"
 #include "List_Three.h"
+#include <sstream>
+#include "StreamTable.h"
 
 using namespace std;
 
@@ -12,7 +14,31 @@ using Three_nsp::Three;
 using Three_nsp::List_Three;
 using Three_nsp::Node;
 
+void printStreamTable1(const List_Three* list)
+{
+	StreamTable st(std::cout);
+	st.AddCol(5);
+	st.AddCol(10);
+	st.AddCol(10);
+	st.AddCol(10);
 
+	//разкомментировать, если столбцы имеют одинаковую толщину
+	//st.Clear();
+	//st.SetCols(4, 10);
+
+	//st.SetVisible(1, false);//столбцы можно скрывать
+
+	st.MakeBorderExt(true);
+	st.SetDelimRow(true, '-');//st.SetDelimRow(false);//без символов-разделителей строк
+	st.SetDelimCol(true, '|');//st.SetDelimCol(false);//без символов-разделителей строк
+
+	//заголовок и значения выводятся одинаково
+	st << "№" << "size" << "end_vertex" << "Alfa";
+	//enum { nr = 10 };
+	for (int i = 0; i < list->size(); i++) {
+		st <<i << list->at(i)->get_size() << list->at(i)->count_end_vertex() << list->at(i)->alfa();
+	}
+}
 
 
 int main()
@@ -30,42 +56,16 @@ int main()
 	Three* derevo;
 		for (int i = 0; i < 400; ++i)
 		{
-			derevo = new Three_nsp::Three(20);
+			derevo = new Three_nsp::Three(200);
 			list->push_back(derevo);
 		}
 		double t = 0;
-		for (int i = 0; i < list->size(); ++i)
-		{
-			t += list->at(i)->expected_val();
-		}
-		double r = t / list->size();
+	printStreamTable1(list);
+	
 
 
-	cout << "ok"<<endl;
-	cout << "size= ";
-	for (int i = 0; i < list->size(); ++i)
-	{
-		cout << list->at(i)->get_size() << " , ";
-	}
-
-	cout << endl;
-
-	cout << "count_end_vertex= ";
-	for (int i = 0; i < list->size(); ++i)
-	{
-		cout << list->at(i)->count_end_vertex()<<" , ";
-	}
-
-	cout << endl;
 	cout << "alfa= ";
-	double alfa = 0;
-	cout << endl;
-	for (int i = 0; i < list->size(); ++i)
-	{
-		alfa += list->at(i)->alfa();
-	}
-	double res = alfa / list->size();
-	cout << res;
+	cout << list->midlle_Alfa();
 	return 0;
 
 }
